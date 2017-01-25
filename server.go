cac21cb887dd1ef75e44b6be6528db69cd4429d8
中澤　一星 <ichamax2@gmail.com>
2017-01-25 19:50:46 +0900

    Path Parameter取得メソッド作成  - getUser()
    Query Parameter取得メソッド作成
     - show()

--- server.go
+++ server.go
@@ -14,5 +14,20 @@ func main() {
     return c.String(http.StatusOK, "Hello World!")
   })
   // サーバ起動
+  e.GET("/users/:id", getUser)
+  e.GET("/show", show)
   e.Logger.Fatal(e.Start(":1323")) // ポード番号指定してね
+}
+
+// /users/joe
+func getUser(c echo.Context) error {
+  id := c.Param("id")
+  return c.String(http.StatusOK, id)
+}
+
+// /show?team=Tigers&member=Yamada
+func show(c echo.Context) error {
+  team := c.QueryParam("team")
+  member := c.QueryParam("member")
+  return c.String(http.StatusOK, "Our Team is " + team + " and one of our member is " + member)
 }
