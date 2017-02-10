37b3088081c8f67445edd4e730ebd5a8e51ca7c8
中澤　一星 <ichamax2@gmail.com>
2017-02-10 16:16:44 +0900

    Form mutipart/form-data done  - saveimage()

--- server.go
+++ server.go
@@ -3,6 +3,8 @@ package main
 import(
   "net/http"
   "github.com/labstack/echo"
+  "io"
+  "os"
 )
 
 func main() {
@@ -16,6 +18,8 @@ func main() {
   // サーバ起動
   e.GET("/users/:id", getUser)
   e.GET("/show", show)
+  e.POST("/save", save)
+  e.POST("/saveimage", saveimage)
   e.Logger.Fatal(e.Start(":1323")) // ポード番号指定してね
 }
 
@@ -30,4 +34,42 @@ func show(c echo.Context) error {
   team := c.QueryParam("team")
   member := c.QueryParam("member")
   return c.String(http.StatusOK, "Our Team is " + team + " and one of our member is " + member)
+}
+
+// e.Post("/save", save)
+func save(c echo.Context) error {
+  // Get Name and Email
+  name := c.FormValue("name")
+  email := c.FormValue("email")
+  return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+}
+
+func saveimage(c echo.Context) error {
+  // Get name
+  name := c.FormValue("name")
+  // Get avatar
+  avatar, err := c.FormFile("avatar")
+  if err != nil {
+    return err
+  }
+
+  // Source
+  src, err := avatar.Open()
+  if err != nil {
+    return err
+  }
+  defer src.Close()
+
+  // Destination
+  dst, err := os.Create(avatar.Filename)
+  if err != nil {
+    return err
+  }
+  defer dst.Close()
+
+  // Copy
+  if _, err = io.Copy(dst, src); err != nil {
+    return err
+  }
+  return c.HTML(http.StatusOK, "<b> thank" + name + " for your " + avatar.Filename + "</b>")
 }
